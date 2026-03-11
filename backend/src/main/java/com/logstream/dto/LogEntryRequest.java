ce0f3219c340176bf38b97839ce71fa7d7deb170
Jeremiah Anku Coblah <jeremiah.coblah@amalitech.com>
2026-03-11 13:39:17 +0000

    refactor: made some edit on the logentry request java file, in order to be able to fix the git merge or conflict errors

--- backend/src/main/java/com/logstream/dto/LogEntryRequest.java
+++ backend/src/main/java/com/logstream/dto/LogEntryRequest.java
@@ -1,8 +1,8 @@
 package com.logstream.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
